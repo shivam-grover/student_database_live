38c928450dbbcdae3780a136477e8e2662d5d8c8
shivumgrover <shivumgrover99@gmail.com>
2018-08-25 17:10:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/fgw/bvceo/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.fgw.bvceo;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    AnimationDrawable animationDrawable;
+    LottieAnimationView animationView;
+    FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+
+
+    DatabaseReference databaseReference;
+
+    // add here
+    @BindView(R.id.email)
+    EditText email;
+
+    @BindView(R.id.password)
+    EditText password;
+
+    @BindView(R.id.signup)
+    TextView signup;
+
+//    @BindView(R.id.progressBar)
+//    ProgressBar progressBar;
+
+    @BindView(R.id.signin)
+    ImageView signin;
+
+    //end here
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+
+        mAuth = FirebaseAuth.getInstance ();
+        animationView.setVisibility(View.GONE);
+//        progressBar.setVisibility(View.GONE);
+        mDatabase = FirebaseDatabase.getInstance();
+        databaseReference = FirebaseDatabase.getInstance ().getReference ("users");
+
+
+
+    }
+    @OnClick(R.id.signin)
+    public void count()
+    {
+        final String emailstring = email.getText ().toString ().trim ();
+
+        // to get Password from user and store it in variable called Password
+        final String PassWord = password.getText ().toString ().trim ();
+
+        //to check editText of email should not be empty
+        //if it is empty then
+        if(emailstring.isEmpty ())
+        {
+            //set an error
+            email.setError ("Email is required");
+            //and highlight that box
+            email.requestFocus ();
+            return;
+        }
+        if(PassWord.isEmpty ())
+        {
+            //set an error
+            password.setError ("Password is required");
+            //it will focus on password
+            password.requestFocus ();
+            return;
+        }
+        if(PassWord.length ()<6)
+        {
+            password.setError ("Minimum length of password required is 6");
+            password.requestFocus ();
+            return;
+        }
+//        progressBar.setVisibility (View.VISIBLE);
+        animationView.setVisibility(View.VISIBLE);
+        animationView.playAnimation();
+        if(android.util.Patterns.EMAIL_ADDRESS.matcher(emailstring).matches())
+        {
+            mAuth.signInWithEmailAndPassword (emailstring,PassWord).addOnCompleteListener (new OnCompleteListener<AuthResult>( ) {
+
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+//                    progressBar.setVisibility (View.GONE);
+                    if (task.isSuccessful ())
+                    {
+                        //if login is successful then
+
+                        Intent intent = new Intent (MainActivity.this, Main3Activity.class);
+                        intent .addFlags (intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity (intent);
+                        Toast.makeText (getApplicationContext (),"log in",Toast.LENGTH_SHORT).show ();
+
+                    }else
+                    {
+                        //else
+                        Toast.makeText (getApplicationContext (), task.getException ().getMessage (),Toast.LENGTH_SHORT).show ();
+                    }
+                }
+            });
+
+        }
+        else{
+
+            databaseReference.child(emailstring).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if(dataSnapshot!=null){
+                        String userId = dataSnapshot.getValue(String.class);
+                        Toast.makeText (getApplicationContext (), userId,Toast.LENGTH_SHORT).show ();
+
+                        mAuth.signInWithEmailAndPassword (userId,PassWord).addOnCompleteListener (new OnCompleteListener<AuthResult>( ) {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                progressBar.setVisibility (View.GONE);
+                                animationView.setVisibility(View.GONE);
+                                animationView.pauseAnimation();
+                                if (task.isSuccessful ())
+                                {
+                                    //if login is successful then
+
+                                    Intent intent = new Intent (MainActivity.this, Main3Activity.class);
+                                    intent .addFlags (intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity (intent);
+                                    Toast.makeText (getApplicationContext (),"log in",Toast.LENGTH_SHORT).show ();
+
+                                }else
+                                {
+                                    //else
+                                    Toast.makeText (getApplicationContext (), task.getException ().getMessage (),Toast.LENGTH_SHORT).show ();
+                                }
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+        //for sign in with valid email address and Password
+
+    }
+
+    @OnClick(R.id.signup)
+    public void setSignup(){
+
+        startActivity (new Intent (this, SignUp.class));
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/fgw/bvceo/SignUp.java
@@ -0,0 +1,139 @@
+package com.example.fgw.bvceo;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class SignUp extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+
+
+    DatabaseReference databaseReference;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //
+
+    @BindView(R.id.username)
+    EditText username;
+
+    @BindView(R.id.email2)
+    EditText email2;
+
+    @BindView(R.id.password2)
+    EditText password2;
+
+    @BindView(R.id.password3)
+    EditText password3;
+
+    @BindView(R.id.signup)
+    ImageView signup;
+
+    @BindView(R.id.already)
+    TextView already;
+
+    @BindView(R.id.progressBar2)
+    ProgressBar progressBar;
+
+
+
+    //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        progressBar.setVisibility(View.GONE);
+        mAuth = FirebaseAuth.getInstance ();
+        mDatabase = FirebaseDatabase.getInstance();
+        databaseReference = FirebaseDatabase.getInstance ().getReference ("users");
+
+
+    }
+
+
+    @OnClick(R.id.signup)
+    public void setSignupa()
+    {
+        final String email = email2.getText ().toString ().trim ();
+        final String PassWord = password2.getText ().toString ().trim ();
+        String PassWord2 = password3.getText ().toString ().trim ();
+        final String user = username.getText().toString();
+
+        if(email.isEmpty ())
+        {
+            email2.setError ("Email is required");
+            email2.requestFocus ();
+            return;
+        }
+        if(PassWord.isEmpty ())
+        {
+            password2.setError ("Password is required");
+            password2.requestFocus ();
+            return;
+        }
+        if(PassWord.length ()<6)
+        {
+            password2.setError ("Minimum length of password required is 6");
+            password2.requestFocus ();
+            return;
+        }
+        if(!(PassWord.equals(PassWord2)))
+        {
+            Toast.makeText(this, "Password does match can't", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressBar.setVisibility (View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword (email , PassWord).addOnCompleteListener (new OnCompleteListener<AuthResult> ( ) {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility (View.GONE);
+                if(task.isSuccessful ())
+                {
+                    databaseReference.child(user).setValue (email);
+
+                    Intent intent = new Intent (SignUp.this, Main3Activity.class);
+                    intent .addFlags (intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity (intent);
+                    Toast.makeText (getApplicationContext (),"You registered successfully",Toast.LENGTH_SHORT).show ();
+                }
+                else {
+                    if (task.getException ( ) instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText (getApplicationContext ( ), "You are already registered", Toast.LENGTH_SHORT).show ( );
+                    } else
+                    {
+                        Toast.makeText (getApplicationContext (),task.getException ().getMessage (),Toast.LENGTH_SHORT).show ();
+                    }
+                }
+            }
+
+        });
+
+
+
+    }
+}
